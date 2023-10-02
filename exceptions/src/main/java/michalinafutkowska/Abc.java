30798671f21be74c632785417e5118b35c25233d
Michalina Futkowska <mfutkowska1@gmail.com>
2023-10-02 18:04:51 +0200

    JIRA-283-adding_new_user - 213 to 123

--- exceptions/src/main/java/michalinafutkowska/Abc.java
+++ exceptions/src/main/java/michalinafutkowska/Abc.java
@@ -1,9 +1,9 @@
 package michalinafutkowska;
 
 public class Abc {
-    private int numb213;
+    private int numb123;
 
     public Abc(int numb) {
-        this.numb213 = numb;
+        this.numb123 = numb;
     }
 }
